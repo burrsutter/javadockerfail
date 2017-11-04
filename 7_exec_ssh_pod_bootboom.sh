5afe7e724e69a3998b9db3f103c119fff6638e12
burrsutter <burrsutter@gmail.com>
2017-11-04 21:37:23 +0200

    better way to get the right pod

--- 7_exec_ssh_pod_bootboom.sh
+++ 7_exec_ssh_pod_bootboom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POD=$(kubectl get pods --namespace=javademo -o yaml | grep "name: bootboom-" | cut -f2 -d':')
+POD=$(kubectl get pods -n javademo -l app=bootboom -o 'jsonpath={.items[0].metadata.name}')
 echo $POD
 command1="kubectl exec -it --namespace=javademo $POD /bin/bash"
 $command1
--- 7_exec_ssh_pod_myvertx.sh
+++ 7_exec_ssh_pod_myvertx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POD=$(kubectl get pods --namespace=javademo -o yaml | grep "name: myvertx-" | cut -f2 -d':')
+POD=$(kubectl get pods -n javademo -l app=myvertx -o 'jsonpath={.items[0].metadata.name}')
 echo $POD
 command1="kubectl exec -it --namespace=javademo $POD /bin/bash"
 $command1
