263f9f11e81c8f4bf9431034875d00e403053f12
burrsutter <burrsutter@gmail.com>
2017-12-12 00:26:46 -0500

    tweaking scripts for better demoability

--- 11_delete_deployment_bootboom.sh
+++ 11_delete_deployment_bootboom.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "Delete the Deployment"
 command1="kubectl delete deploy/bootboom --namespace=javademo"
+echo $command1
+$command1
+
+echo "and delete Service"
+
+command1="kubectl delete svc/bootboom --namespace=javademo"
 echo $command1
 $command1
--- 11_delete_deployment_myvertx.sh
+++ 11_delete_deployment_myvertx.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "Delete the Deployment"
+
 command1="kubectl delete deploy/myvertx --namespace=javademo"
+echo $command1
+$command1
+
+echo "and delete Service"
+
+
+command1="kubectl delete svc/myvertx --namespace=javademo"
 echo $command1
 $command1
